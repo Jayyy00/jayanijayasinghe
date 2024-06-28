cbe3d7ee8b7b719c705ad02c8056c69e1c158e45
J.A Jayani Jayasinghe <99647114+Jayyy00@users.noreply.github.com>
2024-06-29 01:21:39 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,212 @@
+export const Bio = {
+  name: "Jayani Jayasinghe",
+  roles: [
+    "Full Stack Developer",
+      "UI/UX Designer",
+      "Web Developer",
+      "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Jayyy00",
+  resume:
+    "https://drive.google.com/file/d/18lcaItfjtfJk6VSGJjJhdQ6pZ21hCQQw/view?usp=sharing",
+  linkedin: "http://www.linkedin.com/in/jayanijay",
+  twitter: "https://github.com/Jayyy00",
+  insta: "https://www.instagram.com/jayani_jayasinghe_?igsh=eDNobnpybHNudHIw&utm_source=qr",
+  facebook: "https://www.facebook.com/profile.php?id=100089377937513&mibextid=LQQJ4d",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Tailwindcss",
+        image:
+          "https://cdn.icon-icons.com/icons2/2699/PNG/512/tailwindcss_logo_icon_167923.png",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://miro.medium.com/v2/resize:fit:640/format:webp/1*Yia8fURbYbH431G0KxzCXg.png",
+      },
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1400/1*i2fRBk3GsYLeUk_Rh7AzHw.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://c8.alamy.com/zooms/9/8284b53a607a4c8496ed32895c11cab6/2m79r9a.jpg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://i.ytimg.com/vi/RA4Ap1BJ45U/hqdefault.jpg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJdsaOH3LxJisOtkMCdTpqEn-HJmauzZl68A&s",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      {
+          name: "Power BI",
+          image:
+            "https://images.datacamp.com/image/upload/v1714478776/re388xshtgihucfiiavf.png",
+      },
+      {
+          name: "Oracle VM VirtualBox",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAlIGQXZYO0tLMoyZEAst-mv5H1jvk13Vnm7NbWG8BlymGQoynVtWMv0gycs4jWemYSkw&usqp=CAU",
+      },
+      {
+          name: "Mockflow",
+          image:
+            "https://gdm-catalog-fmapi-prod.imgix.net/ProductLogo/c635874d-64b4-4f4b-b1ef-7c5ba449d03e.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://miro.medium.com/v2/resize:fit:1000/0*8v1xNP18Ovj3wg6K.gif",
+    role: "Completed Introduction to MongoDB Course.",
+    desc: "In this course, I learned the basics of MongoDB, including an introduction to NoSQL databases, designing efficient data models, performing create, read, update, and delete (CRUD) operations, improving query performance and data analysis through indexing and aggregation, and ensuring high availability and scalability with replication and sharding. The course provided practical experience through hands-on projects and real-world examples.",
+    doc: "https://drive.google.com/file/d/1OUzRHUPinNaCoMaL6SxjC_Hafr2lFTCN/view?usp=sharing",
+  },
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/6/60/University_of_Moratuwa_logo.png",
+    role: "Completed Web Design for Beginners program by Centre for Open & Distance Learning (CODL) -University of Moratuwa, Sri Lanka.",
+    desc: "In this program, I learned the fundamentals of web design, including an introduction to HTML, CSS, and JavaScript, creating responsive and visually appealing web pages, understanding web standards and best practices, and enhancing user experience with interactive elements. The program emphasized practical experience through hands-on projects and real-world examples, providing a solid foundation in web design principles and techniques.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+];
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSckNJ-tKxk2gfwz5C53cnug_yh2B0nqA229g&s",
+    school: "Sri Lanka Institute of Information Technology (SLIIT)",
+    desc: "I am currently pursuing a BSc. (Hons) in Information Technology, specializing in Information Technology, at Sri Lanka Institute of Information Technology. I am in my 3rd year, 2nd semester. My coursework includes Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among other subjects",
+    degree: "BSc. (Hons) in Information Technology, specializing in Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://images.jobeka.lk/jobs/HreP85uvrCYH30GzAnZ5Jsm4wUDz02M3VyhE66vw.jpg",
+    school: "Buddhist Ladies College, Colombo",
+    desc: "Completed GCE (A/L) - 2020     ||     Completed GCE(O/L) - 2017",
+  
+
+  },
+];
+
+
+
+
